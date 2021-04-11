35cd14f7a16ad3e3d85e98e77ed7edd33d75fc7e
filipeeduardo22 <eduardojs999@gmail.com>
2021-04-11 17:30:22 -0300

    Implementacao da camera acompanhando o player

---
+++ RPG-Companion/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        /*moving character*/
+        Vector3 direction = new Vector3(eixoX, 0, eixoZ);
+        transform.Translate(direction);
+        transform.Translate(direction);
+	}
+}
---
+++ RPG-Companion/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+
+    public Transform target; /*personagem a ser seguido*/
+
+    public float smoothSpeed = 0.125f; /*velocidade da camera*/
+    public Vector3 offset; /*variavel vetorial da camera x,y,z*/
+
+    private void FixedUpdate() /*update feito logo após update normal*/
+    {
+        Vector3 desiredPosition = target.position + offset; /*soma vetorial xyz camera + distancia vetorial desejada*/
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition; 
+    }
+}
